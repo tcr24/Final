ab9e4742e267d3c65041d5954da45e62cd012633
tcr24 <tcr24@njit.edu>
2024-08-02 19:54:02 -0400

    Provide default values for DATABASE_URL, SMTP_USER, and SMTP_PASSWORD

--- app/core/config.py
+++ app/core/config.py
@@ -4,11 +4,11 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
-    DATABASE_URL: str
+    DATABASE_URL: str = "sqlite:///./test.db"
     SMTP_SERVER: str = "localhost"
     SMTP_PORT: int = 25
-    SMTP_USER: str = None
-    SMTP_PASSWORD: str = None
+    SMTP_USER: str = "user@example.com"
+    SMTP_PASSWORD: str = "password"
 
     class Config:
         case_sensitive = True
